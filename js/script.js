67ee0b47a18aba737c9c7097433f07f1592517a5
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-12-06 15:37:30 +0100

    add carousel

--- js/script.js
+++ js/script.js
@@ -25,3 +25,14 @@ const images = [
       text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
    }
 ];
+
+
+const carouselImg = document.getElementById("img");
+let number = 1;
+carouselImg.addEventListener("click", function(){
+    carouselImg.src=`./img/0${number}.webp`;
+    number++;
+    if (number>5){
+        number=1
+    }
+});
